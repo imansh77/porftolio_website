92a9df3baeceb05c2d95c695cf08fa3a6b31992b
iman sharifi <imans494@gmail.com>
2019-04-01 23:36:23 +0430

    blog page is added

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,12 @@ class Blog(models.Model):
 	pub_date = models.DateTimeField()
 	body = models.TextField(max_length=500)
 	image = models.ImageField(upload_to='images/')
+
+	def __str__(self):
+		return self.title
+
+	def summary(self):
+		return list(self.body.split())[:16]
+
+	def pub_date_pretty(self):
+		return self.pub_date.strftime('%b %e %Y')
